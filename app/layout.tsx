435a99d8821039b958908e5e299a0f126d1ae795
UnicornDad8 <unicorndad8@gmail.com>
2024-07-25 11:20:47 -0400

    Setting up project with Next, TypeScript, and Tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Intro section with dropdown navigation",
+  description: "A Junior challenge from front end mentor",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
